1d1b34d5cd44891c37fce2aeadc4e5d0da2b3fa6
Syyad Khan <syykhan@deloitte.com>
2022-07-20 17:22:11 -0400

    sidedrawer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./component/Navbar";
+import Sidenav from "./component/Sidenav";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Sidenav />
+    </>
+  );
+}
+
+export default App;
